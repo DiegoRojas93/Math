e296f012c32d53ef66aaee6b51274acaffb9b06b
Diego Rojas <diegorojas431@gmail.com>
2023-06-20 20:53:53 -0500

    Mediana

--- estadistica.js
+++ estadistica.js
@@ -1,10 +1,31 @@
-function average(array) {
+const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+function average(array) {
+    
     let sum = array.reduce((acum, value) => acum + value )
     
     const average = sum / array.length
     
-    console.log(average);
+    // console.log(average);
 }
 
-average([1, 2, 3, 4, 5, 6, 7, 8, 9  ])
+average(list)
+
+
+function median (array) {
+    let isEven = !(array.length % 2),
+        position,
+        median;
+
+    if (isEven) {
+        position = Math.floor( array.length / 2 )
+        median = (position + (position + 1)) / 2
+        console.log(median);
+    } else {
+        position = Math.floor( array.length / 2 )
+        median = array[position];
+        console.log(median);
+    }
+}
+
+median(list)
